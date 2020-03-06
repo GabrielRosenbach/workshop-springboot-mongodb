1d369dc61272d944869cbee839f82ecf1ff4c30b
Gabriel Rosenbach <gabrielrosenbach@hotmail.com>
2020-03-06 18:22:06 -0300

    Obtendo um usuario por id

--- src/main/java/com/gabrielrosenbach/whorkshopmongo/services/UserService.java
+++ src/main/java/com/gabrielrosenbach/whorkshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.gabrielrosenbach.whorkshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielrosenbach.whorkshopmongo.domain.User;
+import com.gabrielrosenbach.whorkshopmongo.exception.ObjectNotFoundException;
 import com.gabrielrosenbach.whorkshopmongo.repository.UserRepository;
 
 @Service
@@ -17,5 +19,17 @@ public class UserService {
 	public List<User> findAll() {
 		
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		
+		if (!user.isPresent()) {
+			
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return user.get();
 	}
 }
