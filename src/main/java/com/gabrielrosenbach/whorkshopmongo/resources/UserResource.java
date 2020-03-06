67a5038de747c69e55b3e69c911e57e836e23415
Gabriel Rosenbach <gabrielrosenbach@hotmail.com>
2020-03-06 18:46:11 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/gabrielrosenbach/whorkshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielrosenbach/whorkshopmongo/resources/UserResource.java
@@ -8,6 +8,7 @@ import javax.servlet.Servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,5 +55,12 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
