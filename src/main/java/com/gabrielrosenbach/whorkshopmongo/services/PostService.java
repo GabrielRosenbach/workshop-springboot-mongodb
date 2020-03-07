3e4d2def266c78f01629c6d49a8f0a60b3a74036
Gabriel Rosenbach <gabrielrosenbach@hotmail.com>
2020-03-07 19:13:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gabrielrosenbach/whorkshopmongo/services/PostService.java
@@ -0,0 +1,30 @@
+package com.gabrielrosenbach.whorkshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielrosenbach.whorkshopmongo.domain.Post;
+import com.gabrielrosenbach.whorkshopmongo.exception.ObjectNotFoundException;
+import com.gabrielrosenbach.whorkshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		
+		Optional<Post> post = postRepository.findById(id);
+		
+		if (!post.isPresent()) {
+			
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return post.get();
+	}
+	
+}
