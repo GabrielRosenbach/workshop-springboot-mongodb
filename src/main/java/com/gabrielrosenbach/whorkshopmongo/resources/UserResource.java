c9376ec7bccb814e496158edde351c850f718fa8
Gabriel Rosenbach <gabrielrosenbach@hotmail.com>
2020-03-07 19:04:56 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/gabrielrosenbach/whorkshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielrosenbach/whorkshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gabrielrosenbach.whorkshopmongo.domain.Post;
 import com.gabrielrosenbach.whorkshopmongo.domain.User;
 import com.gabrielrosenbach.whorkshopmongo.dto.UserDTO;
 import com.gabrielrosenbach.whorkshopmongo.services.UserService;
@@ -72,5 +73,13 @@ public class UserResource {
 		
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User user = userService.findById(id);
+		
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
