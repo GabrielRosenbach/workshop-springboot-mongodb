d85e74eac926c96d978a2718cbccbf6018e16c04
Gabriel Rosenbach <gabrielrosenbach@hotmail.com>
2020-03-07 21:49:15 -0300

    Consulta com vários critérios

--- src/main/java/com/gabrielrosenbach/whorkshopmongo/config/Instantiation.java
+++ src/main/java/com/gabrielrosenbach/whorkshopmongo/config/Instantiation.java
@@ -1,7 +1,6 @@
 package com.gabrielrosenbach.whorkshopmongo.config;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
--- src/main/java/com/gabrielrosenbach/whorkshopmongo/resources/PostResource.java
+++ src/main/java/com/gabrielrosenbach/whorkshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.gabrielrosenbach.whorkshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,25 +18,41 @@ import com.gabrielrosenbach.whorkshopmongo.services.PostService;
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
-	
+
 	@Autowired
 	private PostService postService;
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
-		
+
 		Post post = postService.findById(id);
-		
+
 		return ResponseEntity.ok().body(post);
 	}
-	
+
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
-		
+
 		text = URL.decodeParam(text);
-		
+
 		List<Post> list = postService.findByTitle(text);
-		
+
 		return ResponseEntity.ok().body(list);
 	}
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+
+		text = URL.decodeParam(text);
+		
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+
+		List<Post> list = postService.fullSearch(text, min, max);
+
+		return ResponseEntity.ok().body(list);
+	}
+
 }
